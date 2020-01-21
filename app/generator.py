c7d944f09cb8f68c1df10b25af6994f9593106ec
Donald Gray <donald.gray@digirati.com>
2020-01-21 16:58:00 +0000

    Group workbook data by MHS_NUMBER
    This is a precursor to generating iiif manifest

--- app/generator.py
+++ app/generator.py
@@ -1,6 +1,7 @@
 import xlrd
 import unicodedata
-from iiif_prezi.factory import ManifestFactory
+
+from app.image_processor import ImageProcessor
 
 # take these as vars
 start_row = 7
@@ -15,41 +16,33 @@ def process_workbook():
     workbook = xlrd.open_workbook(r"C:\temp\Maldives\Maldives.xlsx")
     worksheet = workbook.sheet_by_index(0)
 
-    first_row = column_headers.split(",")
+    headers = column_headers.split(",")
 
     # transform the workbook to a list of dictionary
     data = []
     for row in range(start_row, end_row):
-        image_record = {}
-        for col in range(worksheet.ncols):
+        image_record = {
+            headers[0]: unicodedata.normalize("NFKD", worksheet.cell_value(row, 0)).replace(" ", "")
+        }
+
+        for col in range(1, worksheet.ncols):
             value = worksheet.cell_value(row, col)
             if isinstance(value, str):
-                image_record[first_row[col]] = unicodedata.normalize("NFKD", value)
+                image_record[headers[col]] = unicodedata.normalize("NFKD", value)
             else:
-                image_record[first_row[col]] = value
+                image_record[headers[col]] = value
 
         data.append(image_record)
 
     return data
 
 
-def process_images(data):
-    # TODO group images by id
-    fac = ManifestFactory()
-
-    # Where the resources live on the web
-    # fac.set_base_prezi_uri("https://maldivesheritage.oxcis.ac.uk/object/")
-
-    # Where the resources live on disk
-    fac.set_base_prezi_dir(r"c:\temp\maldives\output")
-
-    fac.set_base_image_uri("https://maldivesheritage.oxcis.ac.uk/image/api/")
-    fac.set_iiif_image_info(2.1, 1)  # Version, ComplianceLevel
-
-
 def main():
     # process the workbook to get data
     data = process_workbook()
+
+    image_processor = ImageProcessor()
+    image_processor.generate_iiif_resources(data)
 
     # iterate through dictionary
     # get image file for current entry
