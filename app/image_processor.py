23ad9f6db5fcfbd6f5091843337a56bd0a2ba530
Donald Gray <donald.gray@digirati.com>
2020-01-22 16:00:23 +0000

    Add basic tile generation. WIP

--- app/image_processor.py
+++ app/image_processor.py
@@ -1,51 +1,53 @@
 from iiif_prezi.factory import ManifestFactory
 from itertools import groupby
+from iiif.static import IIIFStatic
 
 
 class ImageProcessor:
     def __init__(self):
-        self.fac = ManifestFactory()
+        self._manifest_factory = ManifestFactory()
+        self._tile_generator = IIIFStatic(dst=r"c:\temp\maldives\output")
 
         # Where the resources live on the web
-        self.fac.set_base_prezi_uri("https://maldivesheritage.oxcis.ac.uk/object/")
+        self._manifest_factory.set_base_prezi_uri("https://maldivesheritage.oxcis.ac.uk/object/")
 
         # Where the resources live on disk
-        self.fac.set_base_prezi_dir(r"c:\temp\maldives\output")
+        self._manifest_factory.set_base_prezi_dir(r"c:\temp\maldives\output")
 
-        self.fac.set_base_image_uri("https://maldivesheritage.oxcis.ac.uk/image/api/")
-        self.fac.set_iiif_image_info(2.0, 1)  # Version, ComplianceLevel
+        self._manifest_factory.set_base_image_uri("https://maldivesheritage.oxcis.ac.uk/image/api/")
+        self._manifest_factory.set_iiif_image_info(2.0, 1)  # Version, ComplianceLevel
 
     def generate_iiif_resources(self, data):
         # iterate through the images. Group them first
-        grouped_data = self.get_grouped_data(data)
+        grouped_data = self._get_grouped_data(data)
 
         for image_group in grouped_data:
-            self.process_group(image_group)
+            self._process_group(image_group)
 
-    def get_grouped_data(self, data):
+    def _get_grouped_data(self, data):
         groups = []
 
         # order by MHS_NUMBER, before "."
         # assuming rows are in order post-"." in data
-        so = sorted(data, key=lambda item: self.get_mhs_number_root(item))
-        for key, group in groupby(so, lambda item: self.get_mhs_number_root(item)):
+        so = sorted(data, key=lambda item: self._get_mhs_number_root(item))
+        for key, group in groupby(so, lambda item: self._get_mhs_number_root(item)):
             groups.append(list(group))
 
         return groups
 
-    def get_mhs_number_root(self, item):
+    def _get_mhs_number_root(self, item):
         return item["MHS_NUMBER"].split(".")[0]
 
-    def process_group(self, image_group):
-        manifest = self.create_manifest(image_group[0])
+    def _process_group(self, image_group):
+        manifest = self._create_manifest(image_group[0])
 
-        self.add_canvases(image_group, manifest)
+        self._add_canvases(image_group, manifest)
 
         manifest.toFile(compact=False)
 
-    def create_manifest(self, image):
-        mhs_number = self.get_mhs_number_root(image)
-        manifest = self.fac.manifest(label="Manifest for " + mhs_number, ident=mhs_number)
+    def _create_manifest(self, image):
+        mhs_number = self._get_mhs_number_root(image)
+        manifest = self._manifest_factory.manifest(label="Manifest for " + mhs_number, ident=mhs_number)
 
         # add all the metadata fields manifest
         manifest.set_metadata({k: v for k, v in image.items() if v})
@@ -53,12 +55,13 @@ class ImageProcessor:
 
         return manifest
 
-    def add_canvases(self, images, manifest):
+    def _add_canvases(self, images, manifest):
         seq = manifest.sequence()
 
         for p in range(len(images)):
             # Create a canvas with uri slug of page-1, and label of Page 1
-            cvs = seq.canvas(ident="page-%s" % p, label="Page %s" % p)
+            mhs_number = images[p]["MHS_NUMBER"]
+            cvs = seq.canvas(ident=mhs_number, label="Page %s" % p)
 
             # Create an annotation on the Canvas
             anno = cvs.annotation(ident="page-%s" % p)
@@ -68,7 +71,8 @@ class ImageProcessor:
 
             # Set image height and width, and canvas to same dimensions
             # TODO - read correct images
-            imagefile = r"C:\temp\Maldives\imgs\HAF-IVD-1-MS1.1-P5.JPG" if p % 2 == 0 else r"C:\temp\Maldives\imgs\MHS-MS-1206-P41.jpg"
-            img.set_hw_from_file(imagefile)
+            image_file = r"C:\temp\Maldives\imgs\HAF-IVD-1-MS1.1-P5.JPG" if p % 2 == 0 else r"C:\temp\Maldives\imgs\MHS-MS-1206-P41.jpg"
+            self._tile_generator.generate(image_file, mhs_number)
+            img.set_hw_from_file(image_file)
             cvs.height = img.height
             cvs.width = img.width
