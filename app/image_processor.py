3affcc08773ad10d0075843d979cb988dfac83b1
Donald Gray <donald.gray@digirati.com>
2020-01-22 16:17:15 +0000

    Add logging

--- app/image_processor.py
+++ app/image_processor.py
@@ -1,6 +1,7 @@
 from iiif_prezi.factory import ManifestFactory
 from itertools import groupby
 from iiif.static import IIIFStatic
+import logging
 
 
 class ImageProcessor:
@@ -47,7 +48,8 @@ class ImageProcessor:
 
     def _create_manifest(self, image):
         mhs_number = self._get_mhs_number_root(image)
-        manifest = self._manifest_factory.manifest(label="Manifest for " + mhs_number, ident=mhs_number)
+        logging.debug("creating manifest for %s" % mhs_number)
+        manifest = self._manifest_factory.manifest(label="Manifest for %s" % mhs_number, ident=mhs_number)
 
         # add all the metadata fields manifest
         manifest.set_metadata({k: v for k, v in image.items() if v})
@@ -56,23 +58,27 @@ class ImageProcessor:
         return manifest
 
     def _add_canvases(self, images, manifest):
+        image_count = len(images)
+
+        logging.debug("creating %d canvases" % image_count)
+
         seq = manifest.sequence()
 
-        for p in range(len(images)):
+        for p in range(image_count):
             # Create a canvas with uri slug of page-1, and label of Page 1
-            mhs_number = images[p]["MHS_NUMBER"]
-            cvs = seq.canvas(ident=mhs_number, label="Page %s" % p)
+            image_id = images[p]["MHS_NUMBER"]
+            cvs = seq.canvas(ident=image_id, label="Page %s" % p)
 
             # Create an annotation on the Canvas
-            anno = cvs.annotation(ident="page-%s" % p)
+            annotation = cvs.annotation(ident="page-%s" % p)
 
             # Add Image: http://www.example.org/path/to/image/api/p1/full/full/0/native.jpg
-            img = anno.image("p%s" % p, iiif=True)
+            img = annotation.image("p%s" % p, iiif=True)
 
             # Set image height and width, and canvas to same dimensions
             # TODO - read correct images
             image_file = r"C:\temp\Maldives\imgs\HAF-IVD-1-MS1.1-P5.JPG" if p % 2 == 0 else r"C:\temp\Maldives\imgs\MHS-MS-1206-P41.jpg"
-            self._tile_generator.generate(image_file, mhs_number)
+            self._tile_generator.generate(image_file, image_id)
             img.set_hw_from_file(image_file)
             cvs.height = img.height
             cvs.width = img.width
