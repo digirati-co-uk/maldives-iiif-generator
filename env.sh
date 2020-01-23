b37c364e088ca848550c5ec26b5e586f9f8155fc
Donald Gray <donald.gray@digirati.com>
2020-01-23 15:02:08 +0000

    Add basic dockerfile. WIP

---
+++ env.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+export BASE_URL=https://maldivesheritage.oxcis.ac.uk/
+export IMAGE_FILE_OUTPUT_DIR=/usr/local/app/images
+export MANIFEST_OUTPUT_DIR=/usr/local/app/manifest
+export WORKBOOK=/mount
+export START_ROW=62
+export END_ROW=63
