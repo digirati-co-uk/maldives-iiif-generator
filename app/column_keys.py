b2e7e2c19bd25891461a1431ffed5460e740cfb7
Donald Gray <donald.gray@digirati.com>
2020-01-24 14:42:11 +0000

    Refactor generator logic after recieving final data
    Misunderstanding on how grouping of images was to work.
    Move column names into own class.
    Add class to read images for a particular canvas.

---
+++ app/column_keys.py
@@ -0,0 +1,22 @@
+class ColumnKeys:
+    NO = "NO"
+    MHS_NUMBER = "MHS NUMBER"
+    ALTERNATIVE_NAME = "ALTERNATIVE NAME"
+    PAPER_NUMBER = "PAPER NUMBER"
+    ASSOCIATED_ATOLL = "ASSOCIATED ATOLL"
+    ASSOCIATED_ISLAND = "ASSOCIATED ISLAND"
+    PLACE = "PLACE"
+    SCRIPT = "SCRIPT"
+    LANGUAGE = "LANGUAGE"
+    TYPE = "TYPE"
+    DATE = "DATE"
+    PAGES = "PAGES"
+    HEIGHT = "HEIGHT"
+    WIDTH = "WIDTH"
+    MATERIAL = "MATERIAL"
+    ASSOCIATED_PERSONS = "ASSOCIATED PERSONS"
+    COMMENTS = "COMMENTS"
+
+    @staticmethod
+    def csv_list():
+        return f"{ColumnKeys.NO},{ColumnKeys.MHS_NUMBER},{ColumnKeys.ALTERNATIVE_NAME},{ColumnKeys.PAPER_NUMBER},{ColumnKeys.ASSOCIATED_ATOLL},{ColumnKeys.ASSOCIATED_ISLAND},{ColumnKeys.PLACE},{ColumnKeys.SCRIPT},{ColumnKeys.LANGUAGE},{ColumnKeys.TYPE},{ColumnKeys.DATE},{ColumnKeys.PAGES},{ColumnKeys.HEIGHT},{ColumnKeys.WIDTH},{ColumnKeys.MATERIAL},{ColumnKeys.ASSOCIATED_PERSONS},{ColumnKeys.COMMENTS}"
--- app/image_processor.py
+++ app/image_processor.py
@@ -1,12 +1,15 @@
-import logging
+import time
 
 from iiif_prezi.factory import ManifestFactory
-from itertools import groupby
 from iiif.static import IIIFStatic
+
+from app.column_keys import ColumnKeys
+from app.image_reader import ImageReader
 from app.settings import *
 
 
 class ImageProcessor:
+
     def __init__(self):
         self._manifest_factory = ManifestFactory()
         self._tile_generator = IIIFStatic(dst=IMAGE_FILE_OUTPUT_DIR, prefix=IMAGE_BASE_URL)
@@ -22,71 +25,61 @@ class ImageProcessor:
         self._manifest_factory.set_base_image_uri(IMAGE_BASE_URL)
         self._manifest_factory.set_iiif_image_info(2.0, 1)  # Version, ComplianceLevel
 
-    def generate_iiif_resources(self, data):
-        # iterate through the images. Group them first
-        grouped_data = self._get_grouped_data(data)
+        self._image_reader = ImageReader(IMAGE_SOURCE_DIR)
 
-        for image_group in grouped_data:
-            self._process_group(image_group)
+    def generate_iiif_resources(self, manuscript_data):
+        for manuscript in manuscript_data:
+            self._process_manuscript(manuscript)
 
-    def _get_grouped_data(self, data):
-        groups = []
+    def _process_manuscript(self, manuscript):
+        manifest = self._create_manifest(manuscript)
 
-        # order by MHS_NUMBER, before "."
-        # assuming rows are in order post-"." in data
-        so = sorted(data, key=lambda item: self._get_mhs_number_root(item))
-        for key, group in groupby(so, lambda item: self._get_mhs_number_root(item)):
-            groups.append(list(group))
-
-        return groups
-
-    def _get_mhs_number_root(self, item):
-        return item["MHS_NUMBER"].split(".")[0]
-
-    def _process_group(self, image_group):
-        manifest = self._create_manifest(image_group[0])
-
-        self._add_canvases(image_group, manifest)
+        self._add_canvases(manuscript, manifest)
 
         manifest.toFile(compact=False)
 
-    def _create_manifest(self, image):
-        mhs_number = self._get_mhs_number_root(image)
-        alternative_name = image["ALTERNATIVE_NAME"]
+        # TODO - save to S3
 
-        logging.debug(f"creating manifest for {mhs_number}")
+    def _create_manifest(self, manuscript):
+        mhs_number = manuscript.get(ColumnKeys.MHS_NUMBER)
+        alternative_name = manuscript.get(ColumnKeys.ALTERNATIVE_NAME)
+
+        print(f"creating manifest for {mhs_number}")
         manifest = self._manifest_factory.manifest(label=f"{mhs_number} - {alternative_name}", ident=mhs_number)
 
-        # add all the metadata fields manifest
-        manifest.set_metadata({k: v for k, v in image.items() if v})
+        # add all non-empty fields as metadata
+        manifest.set_metadata({k: v for k, v in manuscript.items() if v})
         manifest.description = alternative_name
-
         return manifest
 
-    def _add_canvases(self, images, manifest):
-        image_count = len(images)
+    def _add_canvases(self, manuscript, manifest):
+        manuscript_images = self._image_reader.get_files_for_manuscript(manuscript)
+        mhs_number = manuscript.get(ColumnKeys.MHS_NUMBER)
 
-        logging.debug(f"creating {image_count} canvases..")
+        image_count = len(manuscript_images)
+
+        print(f"creating {image_count} canvases for {mhs_number}..")
 
         seq = manifest.sequence()
 
         for p in range(image_count):
-            # Create a canvas with uri slug of page-1, and label of Page 1
-            image_id = images[p]["MHS_NUMBER"]
+            image_id = f"{mhs_number}-{p}"
+            start = time.time()
+            print(f"processing {image_id}..")
             cvs = seq.canvas(ident=image_id, label=f"Page {p}")
 
             # Create an annotation on the Canvas
             annotation = cvs.annotation(ident=f"page-{p}")
 
             # set source of image data
-            img = annotation.image(f"{image_id}", iiif=True)
+            img = annotation.image(image_id, iiif=True)
 
-            # Add Image: http://www.example.org/path/to/image/api/p1/full/full/0/native.jpg
             # Set image height and width, and canvas to same dimensions
-            # TODO - read correct images
-            image_file = r"C:\temp\Maldives\imgs\HAF-IVD-1-MS1.1-P5.JPG" if p % 2 == 0 else r"C:\temp\Maldives\imgs\MHS-MS-1206-P41.jpg"
-
+            image_file = manuscript_images[p]
             self._tile_generator.generate(src=image_file, identifier=image_id)
             img.set_hw_from_file(image_file)
             cvs.height = img.height
             cvs.width = img.width
+
+            end = time.time()
+            print(f"processed {image_id} in {end - start} secs")
