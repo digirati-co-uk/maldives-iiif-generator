208ff2ba0252b0e60e62e54775cfdef0bcbb6055
Donald Gray <donald.gray@digirati.com>
2020-01-28 12:02:35 +0000

    Better handling of env_vars

--- app/settings.py
+++ app/settings.py
@@ -1,5 +1,7 @@
 import os
 
+from urllib.parse import urljoin
+
 # Base URL for resources
 BASE_URL = os.environ.get("BASE_URL", "https://maldivesheritage.oxcis.ac.uk/")
 
@@ -7,7 +9,7 @@ BASE_URL = os.environ.get("BASE_URL", "https://maldivesheritage.oxcis.ac.uk/")
 IMAGE_BASE_SLUG = os.environ.get("IMAGE_BASE_SLUG", "/images/api")
 
 # Base URL for IIF image resources
-IMAGE_BASE_URL = BASE_URL + IMAGE_BASE_SLUG
+IMAGE_BASE_URL = urljoin(BASE_URL, IMAGE_BASE_SLUG)
 
 # Output dir for generated image pyramid files
 IMAGE_FILE_OUTPUT_DIR = os.environ.get("IMAGE_FILE_OUTPUT_DIR", r"./output/images/api")
@@ -19,7 +21,7 @@ IMAGE_SOURCE_DIR = os.environ.get("IMAGE_SOURCE_DIR", r"./data/image")
 MANIFEST_BASE_SLUG = os.environ.get("MANIFEST_BASE_SLUG", "/manifest")
 
 # Base URL for manifests
-MANIFEST_BASE_URL = BASE_URL + MANIFEST_BASE_SLUG
+MANIFEST_BASE_URL = urljoin(BASE_URL, MANIFEST_BASE_SLUG)
 
 # Output dir for generated IIIF manifests. This must already exist.
 MANIFEST_OUTPUT_DIR = os.environ.get("MANIFEST_OUTPUT_DIR", r"./output/manifest")
